7ca1241ef8ad83a03aec9678339a76d45b017295
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-11-14 22:51:51 +0900

    Add files via upload

--- blog_bulletin/app/src/main/java/com/example/blog_bulletin/MainActivity.kt
+++ blog_bulletin/app/src/main/java/com/example/blog_bulletin/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.blog_bulletin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val setting = R.id.setting
+
+        setting.setOnClickListener {
+            Toast.makeText(this,"Hello World!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
