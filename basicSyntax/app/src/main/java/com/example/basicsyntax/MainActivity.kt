c9927e6ff5505b61425efaed3a8cf8946780c84f
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-11-19 23:37:01 +0900

    Add files via upload

--- basicSyntax/app/src/main/java/com/example/basicsyntax/MainActivity.kt
+++ basicSyntax/app/src/main/java/com/example/basicsyntax/MainActivity.kt
@@ -10,10 +10,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var myName = "홍길동"
-        var myAge : Int
+        var firstName = "Hong"
+        var secondName = "Gil Dong"
+        var myAge: Int
         myAge = 27
         myAge += 1
 
-        Log.d("basicSyntax", "myName = $myName, myAge = $myAge")
+        Log.d("BasicSyntax", "myName = $myName, myAge = $myAge")
+        Log.d("NameSyntax", "제 이름은 $firstName $secondName"+"입니다.")
+
+
+        //조건문
+        var myNumbers = "1,2,3,4,5,6"
+        var thisWeekNumbers = "5,6,7,8,9,10"
+        if(myNumbers==thisWeekNumbers){
+            Log.d("Lotto", "당첨되었습니다.")
+        }
+        else{
+            Log.d("Lotto", "꽝")
+        }
+
+
     }
 }
