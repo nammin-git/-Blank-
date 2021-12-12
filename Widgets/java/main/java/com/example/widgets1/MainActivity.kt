5a04dafdb24e162290bfa78ed013ca51ac7e30df
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-12-12 11:35:19 +0900

    Add files via upload

---
+++ Widgets/java/main/java/com/example/widgets1/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.widgets1
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.widgets1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("EditText", "현재 입력된 값 = ${s.toString()}")
+            }
+        })
+
+    }
+}
