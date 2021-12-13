b8d45786b231d3e99ede8ac5a358192af8169725
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-12-13 23:02:39 +0900

    Add files via upload

--- WidgetsRadio/java/MainActivity.kt
+++ WidgetsRadio/java/MainActivity.kt
@@ -1,25 +1,23 @@
-package com.example.widgetsradio
+package com.example.widgetscheckbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.widgetsradio.databinding.ActivityMainBinding
+import com.example.widgetscheckbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //to access to our views through viewBinding
-        //We need to create binding object for this activity
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
-        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                R.id.radioApple -> Log.d("RadioButton","사과가 선택되었습니다.")
-                R.id.radioBanana -> Log.d("RadioButton","바나나가 선택되었습니다.")
-                R.id.radioOrange -> Log.d("RadioButton","오렌지가 선택되었습니다.")
-            }
+
+        binding.checkAmericano.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked) Log.d("CheckBox", "아메리카노가 선택되었습니다.")
+            else Log.d("CheckBox","사과가 선택해제되었습니다.")
         }
     }
 }
