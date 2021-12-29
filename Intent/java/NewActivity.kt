dd26fb277e1f7f9283a6b47c6685586e6d6d9134
nammin-git <80144205+nammin-git@users.noreply.github.com>
2021-12-30 00:06:07 +0900

    Add files via upload

--- Intent/java/NewActivity.kt
+++ Intent/java/NewActivity.kt
@@ -1,5 +1,7 @@
 package com.example.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.activity.databinding.ActivityNewBinding
@@ -12,5 +14,12 @@ class NewActivity : AppCompatActivity() {
 
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = ${intent.getIntExtra("from2", 0)}
+
+        binding.btnClose.setOnClickListener {
+            val returnIntent = Intent()
+            returnIntent.putExtra("returnValue", binding.editMessage.text.toString())
+            setResult(Activity.RESULT_OK, returnIntent())
+            finish()
+        }
     }
 }
